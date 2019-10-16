35f34b8edd560da535d535e7a16aa386bd36d6b8
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-15 17:43:47 -0700

    Added code for 'even further study'

--- server.py
+++ server.py
@@ -28,6 +28,73 @@ def start_here():
     """
 
 
+@app.route("/options")
+def options():
+    """Choose compliment or insult"""
+
+    return """
+      <!doctype html>
+        <html>
+          <head>
+            <title>Hey!</title>
+          </head>
+
+          <body>
+            <h1>Hi There!</h1>
+            <h3> Do you want a <a href="http://localhost:5000/compliment">compliment</a> or <a href="http://localhost:5000/diss">diss</a> ?</h3>
+             
+          </body>
+      </html>
+      """
+
+@app.route("/compliment")
+def compliment():
+  """choose a compliment"""
+  return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          <select name="compliment">
+            <option value="Nice">Nice</option>
+            <option value="Good">Good</option>
+          </select>
+          <input type="submit" value="Submit">
+        </form>
+
+      
+      </body>
+    </html>
+    """
+@app.route("/diss")
+def diss():
+  """choose a compliment"""
+  return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      
+      <body>
+      <form action="/greet">
+          <h1>Hi There!</h1>
+          What's your name? <input type="text" name="person">
+          <select name="compliment">
+            <option value="Bad">Bad</option>
+            <option value="Mean">Mean</option>
+          </select>
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
 @app.route("/hello")
 def say_hello():
     """Say hello and prompt for user's name."""
@@ -38,6 +105,7 @@ def say_hello():
       <head>
         <title>Hi There!</title>
       </head>
+      
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
